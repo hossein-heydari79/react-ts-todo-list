a9c873d7e13b06d926cb876fa82658fbabeae2c6
hossein-heydari <hosseinheydari790228@gmail.com>
2021-06-26 17:10:15 +0430

    add node-sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      hossein
     </div>
   );
 }
